f2f30747d08afe3b3e9b4ff93ecf029f03bee746
bryantobing12 <bryantobing0@gmail.com>
2022-02-25 10:23:38 +0700

    Add and import interfaces, component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import { foo } from "@Components/foo";
+import { foo } from '@Components/foo';
+import { FooInterface } from '@Components/foo.interface';
 
 console.log(foo);
 
 // Delete me
 export const Thing = () => {
   return <div>Welcome to your first test package.</div>;
+};
+
+export const FooComponent = (props: FooInterface) => {
+  return <div>FooComponent</div>;
 };
